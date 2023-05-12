ac23d9ded958e1d44c30c497d3a44f849968a521
Felipe Tadeu <souzalima@gmail.com>
2023-05-12 18:49:12 -0300

    added functions

--- 02/ex02/Fixed.cpp
+++ 02/ex02/Fixed.cpp
@@ -71,3 +71,85 @@ bool Fixed::operator<=(const Fixed &src) const
 {
     return this->_value <= src._value;
 }
+
+bool Fixed::operator==(const Fixed &src) const
+{
+    return this->_value == src._value;
+}
+
+bool Fixed::operator!=(const Fixed &src) const
+{
+    return this->_value != src._value;
+}
+
+Fixed &Fixed::operator++()
+{
+    this->_value++;
+    return *this;
+}
+
+Fixed Fixed::operator++(int)
+{
+    Fixed tmp(*this);
+    operator++();
+    return tmp;
+}
+
+Fixed &Fixed::operator--()
+{
+    this->_value--;
+    return *this;
+}
+
+Fixed Fixed::operator--(int)
+{
+    Fixed tmp(*this);
+    operator--();
+    return tmp;
+}
+
+int Fixed::getRawBits(void) const
+{
+    return _value;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    _value = raw;
+}
+
+float Fixed::toFloat() const
+{
+    return static_cast<float>(_value) / (1 << _fractionalBits);
+}
+
+int Fixed::toInt() const
+{
+    return _value >> _fractionalBits;
+}
+
+Fixed &Fixed::min(Fixed &a, Fixed &b)
+{
+    return a < b ? a : b;
+}
+
+const Fixed &Fixed::min(const Fixed &a, const Fixed &b)
+{
+    return a < b ? a : b;
+}
+
+Fixed &Fixed::max(Fixed &a, Fixed &b)
+{
+    return a > b ? a : b;
+}
+
+const Fixed &Fixed::max(const Fixed &a, const Fixed &b)
+{
+    return a > b ? a : b;
+}
+
+std::ostream &operator<<(std::ostream &os, const Fixed &fixedPoint)
+{
+    os << fixedPoint.toFloat();
+    return os;
+}
