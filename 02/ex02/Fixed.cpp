d5fbc695b76c1fad8f55cf6fa43e07ebafdc0a09
Felipe Tadeu <souzalima@gmail.com>
2023-05-12 14:30:31 -0300

    added bool

--- 02/ex02/Fixed.cpp
+++ 02/ex02/Fixed.cpp
@@ -2,28 +2,72 @@
 #include <cmath>
 #include "Fixed.hpp"
 
-Fixed::Fixed(): _value(0){
+Fixed::Fixed() : _value(0)
+{
     std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::Fixed(const int intValue){
+Fixed::Fixed(const int intValue)
+{
     std::cout << "Int constructor called" << std::endl;
     _value = intValue << _fractionalBits;
 }
 
-Fixed::Fixed(const float floatValue){
+Fixed::Fixed(const float floatValue)
+{
     std::cout << "Float constructor called" << std::endl;
     _value = roundf(floatValue * (1 << _fractionalBits));
 }
 
-Fixed::~Fixed(){
+Fixed::~Fixed()
+{
     std::cout << "Destructor called" << std::endl;
 }
 
-Fixed& Fixed::operator=(const Fixed& src){
+Fixed &Fixed::operator=(const Fixed &src)
+{
     std::cout << "Assignation operator called" << std::endl;
-    if(this != &src)
+    if (this != &src)
         _value = src.getRawBits();
     return *this;
 }
 
+Fixed Fixed::operator+(const Fixed &src) const
+{
+    return Fixed(this->toFloat() + src.toFloat());
+}
+
+Fixed Fixed::operator-(const Fixed &src) const
+{
+    return Fixed(this->toFloat() - src.toFloat());
+}
+
+Fixed Fixed::operator*(const Fixed &src) const
+{
+    return Fixed(this->toFloat() * src.toFloat());
+}
+
+Fixed Fixed::operator/(const Fixed &src) const
+{
+    return Fixed(this->toFloat() / src.toFloat());
+}
+
+bool Fixed::operator>(const Fixed &src) const
+{
+    return this->_value > src._value;
+}
+
+bool Fixed::operator<(const Fixed &src) const
+{
+    return this->_value < src._value;
+}
+
+bool Fixed::operator>=(const Fixed &src) const
+{
+    return this->_value >= src._value;
+}
+
+bool Fixed::operator<=(const Fixed &src) const
+{
+    return this->_value <= src._value;
+}
--- 02/ex02/Fixed.hpp
+++ 02/ex02/Fixed.hpp
@@ -3,29 +3,30 @@
 
 #include <iostream>
 
-class Fixed{
+class Fixed
+{
 public:
     Fixed();
     Fixed(const int intValue);
     Fixed(const float floatValue);
-    Fixed(const Fixed& src);
+    Fixed(const Fixed &src);
     ~Fixed();
 
-    Fixed& operator=(const Fixed& src);
-    Fixed operator+(const Fixed& src) const;
-    Fixed operator-(const Fixed& src) const;
-    Fixed operator*(const Fixed& src) const;
-    Fixed operator/(const Fixed& src) const;
-    bool operator>(const Fixed& src) const;
-    bool operator<(const Fixed& src) const;
-    bool operator>=(const Fixed& src) const;
-    bool operator<=(const Fixed& src) const;
-    bool operator==(const Fixed& src) const;
-    bool operator!=(const Fixed& src) const;
-    Fixed& operator++();    // Prefix increment
-    Fixed operator++(int);  // Postfix increment
-    Fixed& operator--();    // Prefix decrement
-    Fixed operator--(int);  // Postfix decrement
+    Fixed &operator=(const Fixed &src);
+    Fixed operator+(const Fixed &src) const;
+    Fixed operator-(const Fixed &src) const;
+    Fixed operator*(const Fixed &src) const;
+    Fixed operator/(const Fixed &src) const;
+    bool operator>(const Fixed &src) const;
+    bool operator<(const Fixed &src) const;
+    bool operator>=(const Fixed &src) const;
+    bool operator<=(const Fixed &src) const;
+    bool operator==(const Fixed &src) const;
+    bool operator!=(const Fixed &src) const;
+    Fixed &operator++();   // Prefix increment
+    Fixed operator++(int); // Postfix increment
+    Fixed &operator--();   // Prefix decrement
+    Fixed operator--(int); // Postfix decrement
 
     int getRawBits() const;
     void setRawBits(int const raw);
@@ -33,17 +34,16 @@ public:
     float toFloat() const;
     int toInt() const;
 
-    static Fixed& min(Fixed& a, Fixed& b);
-    static const Fixed& min(const Fixed& a, const Fixed& b);
-    static Fixed& max(Fixed& a, Fixed& b);
-    static const Fixed& max(const Fixed& a, const Fixed& b);
+    static Fixed &min(Fixed &a, Fixed &b);
+    static const Fixed &min(const Fixed &a, const Fixed &b);
+    static Fixed &max(Fixed &a, Fixed &b);
+    static const Fixed &max(const Fixed &a, const Fixed &b);
 
 private:
     int _value;
     static const int _fractionalBits = 8;
-
 };
 
-std::ostream& operator<<(std::ostream& os, const Fixed& fixedPoint);
+std::ostream &operator<<(std::ostream &os, const Fixed &fixedPoint);
 
 #endif
