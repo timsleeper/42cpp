874cd5f9c681c07cbd1367bfb21f5deef6e917d2
Felipe Tadeu <souzalima@gmail.com>
2023-05-13 05:05:39 +0200

    added more tests

--- 02/ex01/main.cpp
+++ 02/ex01/main.cpp
@@ -8,7 +8,7 @@ int main(void){
     Fixed const d(b);
 
     a = Fixed(1234.4321f);
-
+	std::cout << "--------- Original tests ---------" << std::endl;
     std::cout << "a is " << a << std::endl;
     std::cout << "b is " << b << std::endl;
     std::cout << "c is " << c << std::endl;
@@ -18,6 +18,42 @@ int main(void){
     std::cout << "b is " << b.toInt() << " as a integer" << std::endl;
     std::cout << "c is " << c.toInt() << " as a integer" << std::endl;
     std::cout << "d is " << d.toInt() << " as a integer" << std::endl;
+
+	std::cout << "--------- Other tests ---------" << std::endl;
+	
+	// Test the constructors
+    Fixed defaultFixed;
+    std::cout << "Default Fixed: " << defaultFixed << std::endl;
+    
+    Fixed intFixed(42);
+    std::cout << "Int Fixed: " << intFixed << std::endl;
+    
+    Fixed floatFixed(3.14159f);
+    std::cout << "Float Fixed: " << floatFixed << std::endl;
+    
+    Fixed copyFixed(floatFixed);
+    std::cout << "Copy Fixed: " << copyFixed << std::endl;
+    
+    // Test the assignment operator
+    Fixed assignedFixed = intFixed;
+    std::cout << "Assigned Fixed: " << assignedFixed << std::endl;
+    
+    // Test getRawBits and setRawBits
+    int rawBits = intFixed.getRawBits();
+    std::cout << "Raw Bits: " << rawBits << std::endl;
+    
+    copyFixed.setRawBits(rawBits);
+    std::cout << "Copy with Set Raw Bits: " << copyFixed << std::endl;
+    
+    // Test toFloat and toInt
+    float floatValue = floatFixed.toFloat();
+    std::cout << "Float Value: " << floatValue << std::endl;
+    
+    int intValue = intFixed.toInt();
+    std::cout << "Int Value: " << intValue << std::endl;
+    
+    // Test the ostream operator
+    std::cout << "Fixed Point: " << floatFixed << std::endl;
 
     return 0;
 }
