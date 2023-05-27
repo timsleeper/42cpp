1af488d036e08958d928a7cb35417556599d33c5
Felipe Tadeu <souzalima@gmail.com>
2023-05-27 16:17:11 -0300

    added tests. ex02 done

--- 05/ex02/main.cpp
+++ 05/ex02/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-#include "Bureaucrat.hpp"
-#include "PresidentialPardonForm.hpp"
+#include "Bureaucrat.h"
+#include "PresidentialPardonForm.h"
+#include "RobotomyRequestForm.h"
+#include "ShrubberyCreationForm.h"
 
 
 int main() {
@@ -14,7 +16,7 @@ int main() {
         std::cout << e.what() << std::endl;
     }
 
-    std::cout << "Testing creating a Bureau with grade 151" << std::endl;
+    std::cout << "Testing creating a Bureau with grade -1" << std::endl;
     try{
         Bureaucrat Lincoln("Lincoln", -1);
     } catch (std::exception &e) {
@@ -25,6 +27,10 @@ int main() {
     Bureaucrat Obama("Obama", 1);
     std::cout << Obama;
 
+    std::cout << "Testing creating a Bureaucrat ObamaLow with grade 150" << std::endl;
+    Bureaucrat ObamaLow("Obama", 150);
+    std::cout << ObamaLow;
+
     std::cout << "Incrementing grade already at maximum (1)" << std::endl;
     try {
         Obama.incrementGrade();
@@ -32,6 +38,13 @@ int main() {
         std::cout << e.what() << std::endl;
     }
     std::cout << Obama;
+
+    std::cout << "Decrementing grade already at minimum (150)" << std::endl;
+    try {
+        ObamaLow.decrementGrade();
+    } catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
 
     std::cout << "Testing decrementing grade. Should be (2)" << std::endl;
     try {
@@ -41,6 +54,7 @@ int main() {
     }
     std::cout << Obama;
 
+    std::cout << "PresidentialPardonForm: Required grades: sign 25, exec 5" << std::endl;
     PresidentialPardonForm form("Hillary Clinton");
     try {
         Obama.signForm(form);
@@ -49,7 +63,36 @@ int main() {
         std::cout << e.what() << std::endl;
     }
 
+    for (int i = Obama.getGrade(); i < 6; i++) {
+        Obama.decrementGrade();
+    }
+
+    std::cout << "PresidentialPardonForm: Testing grade to low to execute:" << std::endl;
+    try {
+        Obama.signForm(form);
+        Obama.executeForm(form);
+    } catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+
     std::cout << "ShrubberyCreationForm: Required grades: sign 145, exec 137" << std::endl;
+    ShrubberyCreationForm form2("Greenpeace");
+    try {
+        Obama.signForm(form2);
+        Obama.executeForm(form2);
+    } catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+
+    std::cout << "RobotomyRequestForm: Required grades: sign 72, exec 45" << std::endl;
+    RobotomyRequestForm form3("DeathRowInmate");
+    try {
+        Obama.signForm(form3);
+        Obama.executeForm(form3);
+    } catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+
     return 0;
 
 }
